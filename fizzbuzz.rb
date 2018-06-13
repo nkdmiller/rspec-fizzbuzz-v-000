66e64664283a451520f19d39e5481f9292c819c6
Nate Miller <natekmiller96@gmail.com>
2018-06-13 15:38:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -9,7 +9,7 @@ def fizzbuzz(the_number)
   elsif (the_number / 3).class == Fixnum
     puts "Fizz"
   end
-
+end
 puts"it puts the lotion in the basket"
 the_number=gets.strip
 the_number=the_number.to_i
